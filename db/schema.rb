e66f4d449d36b50a75504b40beb00cadca616c78
Janet Ferguson <janet.ferguson@me.com>
2015-06-13 00:12:30 +0000

    Created tasks scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612003224) do
+ActiveRecord::Schema.define(version: 20150613000732) do
+
+  create_table "tasks", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
